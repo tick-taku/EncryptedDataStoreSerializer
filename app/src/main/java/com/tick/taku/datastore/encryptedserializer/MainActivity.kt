c301e8c82c42b252e3ef51981a4993f1bfe3fa11
tick-taku <tick.taku.77@gmail.com>
2022-02-14 18:41:56 +0900

    Initial commit

---
+++ app/src/main/java/com/tick/taku/datastore/encryptedserializer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tick.taku.datastore.encryptedserializer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
