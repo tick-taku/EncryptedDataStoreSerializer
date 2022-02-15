889c61441d258ab9a94a7760f1951ee48aaeab24
tick-taku <tick.taku.77@gmail.com>
2022-02-15 15:58:14 +0900

    Create ViewModel

---
+++ app/src/main/java/com/tick/taku/datastore/encryptedserializer/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.tick.taku.datastore.encryptedserializer
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tick.taku.datastore.encryptedserializer.datasource.TextPreferencesDataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val dataStore: TextPreferencesDataStore
+): ViewModel() {
+
+    data class UiState(
+        val text: String = ""
+    )
+
+    val uiState: StateFlow<UiState> = dataStore.load().map {
+        UiState(text = it)
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), UiState())
+
+    fun save(text: String) {
+        viewModelScope.launch {
+            dataStore.save(text)
+        }
+    }
+}
